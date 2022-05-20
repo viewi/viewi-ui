1c3e7ce9fbdb51139aad090e7b716450812f4b0f
Ivan Voitovych <col606v@gmail.com>
2022-05-20 10:19:37 +0300

    Package: getAssetsPath

--- src/ViewiUI/Package.php
+++ src/ViewiUI/Package.php
@@ -12,47 +12,13 @@ class Package extends ViewiPackage
         return __DIR__ . DIRECTORY_SEPARATOR . 'Components';
     }
 
-    public function onBuild(PageEngine $pageEngine): void
+    public function getAssetsPath(): ?string
     {
-        $assetsDestinationPath = $pageEngine->getConfig()[PageEngine::PUBLIC_ROOT_DIR];
-        $assetsSourceDir = __DIR__ . DIRECTORY_SEPARATOR . 'public';
-        // copy all assets
-        $assets = [];
-        $this->getDirContents($assetsSourceDir, $assets);
-        foreach ($assets as $path => $type) {
-            $basePath = str_replace($assetsSourceDir, '', $path);
-            $destinationPath = $assetsDestinationPath . $basePath;
-            // $pageEngine->debug([$type, $assetsSourceDir, $path, $basePath, $assetsDestinationPath, $destinationPath]);
-            switch ($type) {
-                case 'folder': {
-                        if (!file_exists($destinationPath)) {
-                            mkdir($destinationPath, 0777, true);
-                        }
-                        break;
-                    }
-                case 'file':
-                default: {
-                        // file
-                        file_put_contents($destinationPath, file_get_contents($path));
-                    }
-            }
-        }
+        return __DIR__ . DIRECTORY_SEPARATOR . 'public';
     }
 
-    private function getDirContents($dir, &$results = array())
+    public function onBuild(PageEngine $pageEngine): void
     {
-        $files = scandir($dir);
-
-        foreach ($files as $key => $value) {
-            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
-            if (!is_dir($path)) {
-                $results[$path] = 'file';
-            } else if ($value != "." && $value != "..") {
-                $results[$path] = 'folder';
-                $this->getDirContents($path, $results);
-            }
-        }
-
-        return $results;
+        // nothing
     }
 }
