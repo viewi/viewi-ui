cd726e9f390d49c37697d59f7b38d528f3f2c45b
Ivan Voitovych <col606v@gmail.com>
2022-06-14 17:20:25 +0300

    Column component

--- src/ViewiUI/Components/UI/Grid/Column.php
+++ src/ViewiUI/Components/UI/Grid/Column.php
@@ -7,7 +7,7 @@ use Viewi\BaseComponent;
 class Column extends BaseComponent
 {
 
-    public int $cols = 12;
+    public ?int $cols = null;
     public ?int $xs = null;
     public ?int $sm = null;
     public ?int $md = null;
@@ -17,12 +17,24 @@ class Column extends BaseComponent
 
     public function getClasses()
     {
-        return 'col-' . $this->cols
+        $breakpointClasses = '';        
+        foreach ($this->_props as $prop => $value) {
+            if (
+                strpos($prop, 'offset-') === 0
+                || strpos($prop, 'order-') === 0
+            ) {
+                $breakpointClasses .= " $prop-$value";
+            }
+        }
+        $hasColClasses = $this->cols || $this->xs || $this->sm || $this->md || $this->lg || $this->xl;
+        return ($hasColClasses ? '' : 'col')
+            . ($this->cols ? 'col-' . $this->cols : '')
             . ($this->alignSelf ? ' align-self-' . $this->alignSelf : '')
             . ($this->xs !== null ? ' col-xs-' . $this->xs : '')
             . ($this->sm !== null ? ' col-sm-' . $this->sm : '')
             . ($this->md !== null ? ' col-md-' . $this->md : '')
             . ($this->lg !== null ? ' col-lg-' . $this->lg : '')
-            . ($this->xl !== null ? ' col-xl-' . $this->xl : '');
+            . ($this->xl !== null ? ' col-xl-' . $this->xl : '')
+            . $breakpointClasses;
     }
 }
