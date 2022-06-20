0755613c1e8257ac5d98ad0e65316176731b5ab9
Ivan Voitovych <col606v@gmail.com>
2022-06-20 13:05:30 +0300

    Icon component

--- src/ViewiUI/Components/UI/Icons/Icon.php
+++ src/ViewiUI/Components/UI/Icons/Icon.php
@@ -10,12 +10,19 @@ class Icon extends BaseComponent
     public ?string $color = null;
     public string $name;
     public string $tag = 'i';
+    public bool $dense = false;
+    public bool $disabled = false;
     /**
      * 
      * @var '' | 'xsmall' | 'small' | 'large' | 'xlarge'
      */
     public string $size = '';
     public ?string $position = null;
+
+    function __mounted()
+    {
+        $this->tag = $this->clickable() ? 'button' : $this->tag;
+    }
 
     function getClasses()
     {
@@ -25,7 +32,15 @@ class Icon extends BaseComponent
         }
         $classes .= $this->size ? ' size-' . $this->size : '';
         $classes .= $this->position ? ' pos-' . $this->position : '';
+        $classes .= $this->dense ? ' viewi-icon-dense' : '';
+        $classes .= $this->disabled ? ' viewi-icon-disabled' : '';
+        $classes .= $this->clickable() ? ' viewi-icon-link' : '';
         return $classes;
+    }
+
+    function clickable(): bool
+    {
+        return isset($this->_props['(click)']);
     }
 
     function onClick(DOMEvent $event)
