286f41fceb8c0f6ae0ae21d262af80dc449aef21
Ivan Voitovych <col606v@gmail.com>
2022-07-01 10:26:51 +0300

    ViewiUI: minify, shake tree, combine css; fonts

--- src/ViewiUI/Components/Views/Layouts/ViewiUI.php
+++ src/ViewiUI/Components/Views/Layouts/ViewiUI.php
@@ -6,5 +6,24 @@ use Viewi\BaseComponent;
 
 class ViewiUI extends BaseComponent
 {
+    /**
+     * 
+     * @var string[]
+     */
+    public array $icons = ['mdi'];
 
+    public function isMaterial()
+    {
+        return in_array('mdi', $this->icons);
+    }
+
+    public function isFontAwesome4()
+    {
+        return in_array('fa4', $this->icons);
+    }
+
+    public function isFontAwesome5()
+    {
+        return in_array('fa5', $this->icons);
+    }
 }
