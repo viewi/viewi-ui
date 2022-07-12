dc5cf7432d60d5de96a7f87b0d842355546507ab
Ivan Voitovych <col606v@gmail.com>
2022-07-12 18:29:23 +0300

    TextField

--- src/ViewiUI/Components/UI/Forms/TextField.php
+++ src/ViewiUI/Components/UI/Forms/TextField.php
@@ -8,6 +8,7 @@ use Viewi\DOM\Events\DOMEvent;
 class TextField extends BaseComponent
 {
     public string $value = '';
+    public ?string $uid = null;
     public ?string $id = null;
     public string $label = '';
     public ?string $placeholder = null;
@@ -16,20 +17,44 @@ class TextField extends BaseComponent
     public bool $filled = false;
     public bool $outlined = false;
     public bool $booted = false;
+    public bool $hasValue = false;
 
     function __mounted()
     {
         $this->booted = true;
+        $this->uid = $this->__id;
     }
 
     function getId(): string
     {
-        return $this->id ?? "input-{$this->__id}";
+        return $this->id ?? "input-{$this->uid}";
+    }
+
+    function getClasses(): string
+    {
+        $classes = 'viewi-input text-field';
+        $classes .= $this->solo ? ' text-field-solo' : '';
+        $classes .= $this->isEnclosed() ? ' text-field-enclosed' : '';
+        $classes .= $this->filled ? ' text-field-filled' : '';
+        $classes .= $this->outlined ? ' text-field-outlined' : '';
+        return $classes;
     }
 
     function isEnclosed()
     {
         return $this->solo || $this->filled || $this->outlined;
+    }
+
+    function onClick($event)
+    {
+        $this->emitEvent('click', $event);
+        $this->_refs['input']->focus();
+    }
+
+    function onInput(DOMEvent $event)
+    {
+        $this->hasValue = !!$event->target->value;
+        $this->emitEvent('input', $event);
     }
 
     function onFocus($event)
