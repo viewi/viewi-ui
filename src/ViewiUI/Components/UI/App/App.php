69f6b3ba95ef5cf4b0b31ace1b66c2792211740e
Ivan Voitovych <col606v@gmail.com>
2022-08-02 19:10:26 +0300

    App

---
+++ src/ViewiUI/Components/UI/App/App.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ViewiUI\Components\UI\App;
+
+use Viewi\BaseComponent;
+
+class App extends BaseComponent
+{
+    public bool $rtl = false;
+    public ?string $id = null;
+    public bool $dark = false;
+}
